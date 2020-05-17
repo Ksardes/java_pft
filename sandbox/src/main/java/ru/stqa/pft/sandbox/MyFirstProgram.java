f2efd0bfb96adc942f715bee3cbee0def136facd
Artem.spitcin <artem.spitsyn@mobirate.com>
2020-05-18 02:44:26 +0400

    Функции квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,23 +5,15 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
 
-    double l = 5;
-    System.out.println("S " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("S " + s.l + " = " + s.area());
 
-   double a = 5;
-   double b = 10;
-   System.out.println("S with side " + a + " and "+ b + " = " + area(a,b));
+    Rectangle r = new Rectangle(4,10);
+    System.out.println("S with side " + r.a + " and " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello " + somebody);
   }
 
-  public static double area(double l) {
-    return l * l;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
 }
