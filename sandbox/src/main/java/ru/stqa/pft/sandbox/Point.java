d69525dd8287df6de86f43fc433130048fb00116
Artem.spitcin <artem.spitsyn@mobirate.com>
2020-05-18 02:44:08 +0400

    Task 2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x,double y){
+    this.x = x;
+    this.y = y;
+  }
+  public double distance (Point p) {
+    double a = this.x - p.x;
+    double b = this.y - p.y;
+    double l = Math.sqrt(a * a + b * b);
+    double len;
+    return len = l;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sandbox;
+
+import com.sun.scenario.animation.shared.ClipEnvelope;
+
+import java.sql.SQLOutput;
+
+public class Task2 {
+
+  public static void main(String[] args) {
+    Point p1 = new Point(10, 15);
+    Point p2 = new Point(4, 4);
+    System.out.println("distance between Point p1 and Point p2 = " + distance(p1,p2));
+    System.out.println("distance between Point p1 and Point p2 = " + p1.distance(p2));
+  }
+  public static double distance (Point p1,Point p2) {
+    double a = p2.x - p1.x;
+    double b = p2.y - p1.y;
+    double l = Math.sqrt(a * a + b * b);
+    double len;
+    return len=l;
+  }
+}
