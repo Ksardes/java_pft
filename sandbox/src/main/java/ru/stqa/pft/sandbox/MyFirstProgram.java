0c864bc27832c6920870cfc0ff379f85e5f1ed00
Artem.spitcin <artem.spitsyn@mobirate.com>
2020-05-18 01:04:44 +0400

    Добавлены функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    hello("world");
 
-    System.out.println("Hello world");
+    double l = 5;
+    System.out.println("S " + l + " = " + area(l));
+
+   double a = 5;
+   double b = 10;
+   System.out.println("S with side " + a + " and "+ b + " = " + area(a,b));
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody);
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
