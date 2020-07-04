b7fe9c845ea38fb655e469995c09292b54614992
Artem.spitcin <artem.spitsyn@mobirate.com>
2020-07-04 10:39:09 +0400

    Refactor with Hamcrest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -3,28 +3,29 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.List;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class GroupDeletionTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().GroupPage();
-    if (app.group().List().size() == 0) {
+    if (app.group().all().size() == 0) {
       app.group().create(new GroupData().withName("test1"));
     }
   }
 
   @Test
-  public void testGroupDelition() throws Exception {
-    List<GroupData> before = app.group().List();
-    int index = before.size() -1;
-    app.group().delete(index);
-    List<GroupData> after = app.group().List();
+  public void testGroupDeletion() throws Exception {
+    Groups before = app.group().all();
+    GroupData deletedGroup = before.iterator().next();
+    app.group().delete(deletedGroup);
+    Groups after = app.group().all();
     Assert.assertEquals(after.size(),before.size()-1);
 
-    before.remove(index);
-    Assert.assertEquals(before,after);
+    assertThat(after, equalTo(before.without(deletedGroup)));
   }
 }
